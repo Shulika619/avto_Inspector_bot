d52822f5f400fe828a129a3ffa8364fbb0dea195
Sergey Shulika <developer.shulika@gmail.com>
2023-09-25 13:16:42 +0300

    init commit

---
+++ src/main/java/dev/shulika/avto_inspector_bot/AvtoInspectorBotApplication.java
@@ -0,0 +1,13 @@
+package dev.shulika.avto_inspector_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvtoInspectorBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AvtoInspectorBotApplication.class, args);
+    }
+
+}
